5c66cad53b55558f02d1592695f7bb89fede0d8a
Perry Craft <ljcraft12@gmail.com>
2020-08-08 01:25:36 -0400

    Added and commented out view more button for each item. Need to find out what they expect to be able to do with items and size selection.

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,8 @@ window.addEventListener('load', () => {
 
 
 const shortSleeveGallery = document.getElementById('short-sleeve-gallery');
+const loading = document.getElementById('loading');
+const products = document.getElementById('products');
 const longSleeveGallery = document.getElementById('long-sleeve-gallery');
 const blazerGallery = document.getElementById('blazer-gallery');
 
@@ -37,8 +39,8 @@ function getProducts() {
 }
 
 function generateCard(data) {
-   return `
-          <div class="card col-lg-2 m-3">
+    return `
+          <div class="card col-lg-3 offset-lg-1 mb-4">
                  <div class=" card-img-top">
                  <img src=${data.image} alt="${data.name}" class="card-img-top">
              </div>
@@ -47,8 +49,47 @@ function generateCard(data) {
                <p class="card-text">${data.description}.</p>
                <p><b><small>Price </small>$${data.price}</b></p>
              </div>
+<!--               <button class="btn btn-outline-primary mb-4 mt-4" data-id='${JSON.stringify(data)}' data-toggle="modal" data-target="#product-modal" onclick="viewMore(event)">View more</button>-->
         </div>
                  `
+}
+
+function viewMore(e) {
+    let modalDiv = document.querySelector('#product-modal')
+    let {name, id, price, description, image} = JSON.parse(e.target.dataset.id);
+    let modal = `
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${name}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+         <div class="card mb-4">
+                 <div class=" card-img-top">
+                 <img src=${image} alt="${name}" class="card-img-top">
+             </div>
+             <div class="card-body">
+               <p class="card-title text-center"><b>${name}</b></p>
+               <p class="card-text">${description}.</p>
+               <p><b><small>Price </small>$${price}</b></p>
+               <label for="size-select">Size: </label>
+               <select name="size" id="size-select">
+               <option value="s">Small</option>
+               <option value="m">Medium</option>
+               <option value="l">Large</option>
+               <option value="xl">X-Large</option>
+               <option value="xxl">XX-Large</option>
+</select>
+             </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+    modalDiv.innerHTML = modal
 }
 
 getProducts()
